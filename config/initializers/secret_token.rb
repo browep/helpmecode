e5d0a1dd11c938decc2ae53d25cb8ccd496cdf61
paul <paul@paul-Pangolin-Performance.(none)>
2011-11-12 21:04:41 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpmecode::Application.config.secret_token = 'd3634bb1add48dcb8b661c33389f9f6c76f85eec3db911ac4020778af77c3e46676f76e8ec5cfc9927767515741e12216e5d0b743714993640382baba33c57a8'
