1c54536e7ce4541d4d8cb5d065e379605ba8de23
paul <paul@paul-Pangolin-Performance.(none)>
2011-12-05 17:48:05 -0700

    header and code buttons for tutorial creation

--- app/assets/javascripts/markitup/sets/default/set.js
+++ app/assets/javascripts/markitup/sets/default/set.js
@@ -13,7 +13,7 @@ var mySettings = {
 	onShiftEnter:  	{keepDefault:false, replaceWith:'<br />\n'},
 	onCtrlEnter:  	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>'},
 	onTab:    		{keepDefault:false, replaceWith:'    '},
-	markupSet:  [ 	
+	markupSet:  [
 		{name:'Bold', key:'B', openWith:'(!(<strong>|!|<b>)!)', closeWith:'(!(</strong>|!|</b>)!)' },
 		{name:'Italic', key:'I', openWith:'(!(<em>|!|<i>)!)', closeWith:'(!(</em>|!|</i>)!)'  },
 		{name:'Stroke through', openWith:'<del>', closeWith:'</del>' },
@@ -25,6 +25,19 @@ var mySettings = {
 		{name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
 		{separator:'---------------' },
 		{name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/<(.*?)>/g, "") } },		
-		{name:'Preview', key: 'P',className:'preview',  call:'preview'}
+		{name:'Preview', key: 'P',className:'preview',  call:'preview'}                  ,
+        {separator:'---------------' },
+        {name:'Heading 1', key:'1', openWith:'<h1(!( class="[![Class]!]")!)>', closeWith:'</h1>', placeHolder:'Your title here...' },
+        {name:'Heading 2', key:'2', openWith:'<h2(!( class="[![Class]!]")!)>', closeWith:'</h2>', placeHolder:'Your title here...' },
+        {name:'Heading 3', key:'3', openWith:'<h3(!( class="[![Class]!]")!)>', closeWith:'</h3>', placeHolder:'Your title here...' },
+        {name:'Heading 4', key:'4', openWith:'<h4(!( class="[![Class]!]")!)>', closeWith:'</h4>', placeHolder:'Your title here...' },
+        {name:'Heading 5', key:'5', openWith:'<h5(!( class="[![Class]!]")!)>', closeWith:'</h5>', placeHolder:'Your title here...' },
+        {name:'Heading 6', key:'6', openWith:'<h6(!( class="[![Class]!]")!)>', closeWith:'</h6>', placeHolder:'Your title here...' },
+        {name:'Paragraph', openWith:'<p(!( class="[![Class]!]")!)>', closeWith:'</p>'  },
+        {separator:'---------------' },
+        {name:'Code', key:'K', openWith:'<pre(!( class="[![Class]!]")!)>', closeWith:'</pre>', placeHolder:'CODE',className:"code-button" }
+
+
+
 	]
 }
