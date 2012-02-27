b698f1e26a37d8c18353e355535100cb58dafc6f
paul <paul@paul-Pangolin-Performance.(none)>
2012-02-27 00:31:55 -0700

    tutorial requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111212032041) do
+ActiveRecord::Schema.define(:version => 20120226215103) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
     t.integer  "tutorial_id"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "requests", :force => true do |t|
+    t.integer  "user_id"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
